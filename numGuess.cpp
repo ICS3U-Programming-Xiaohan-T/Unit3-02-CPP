15d5371a6e80aab3b8eeecb15f58257a96473d60
Xiaohan Tian <xiaohan.tian@stu.ocsb.ca>
2025-03-19 03:56:44 +0000

    numGuessUpdated.cpp

---
+++ numGuess.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2025 Xiaohan All rights reserved.
+//
+// Created by: Xiaohan
+// Created on: Mar 2025
+// This program checks if users guessed the right numbers
+
+#include <iostream>
+int main() {
+    // this function checks if the guessed number is correct
+    const int CORRECT_NUMBER = 8;
+    int guessedNumber;
+
+    // get input of guessed number from users
+    std::cout << "Enter the number you guess between 1 to 9 ";
+    std::cin >> guessedNumber;
+    std::cout << "" << std::endl;
+
+    // the process of guessing numbers
+    if (guessedNumber != CORRECT_NUMBER) {
+    // display the result to users
+        std::cout << "You guess is incorrect!";
+    } else {
+        // display the result to users
+        std::cout << "You guess is correct!";
+    }
+}
